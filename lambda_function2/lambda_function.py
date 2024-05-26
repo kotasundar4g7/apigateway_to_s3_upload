117f5b3666d6f695477b4ef6c1dce88634f77da0
kotasundar4g7 <kota.reddy@happiestminds.com>
2024-05-26 19:00:13 +0530

    Update lambda_function.py

--- lambda_function2/lambda_function.py
+++ lambda_function2/lambda_function.py
@@ -1,6 +1,6 @@
 import boto3
 import os
-glue_crawler = os.environ['glue_crawler']
+glue_crawler = os.environ['glue_crawler'] # fetching the glue crawler name 
 def lambda_handler(event, context):
-    glue_client = boto3.client('glue')
-    glue_client.start_crawler(Name=glue_crawler)
+    glue_client = boto3.client('glue')# creating boto3 client for glue.
+    glue_client.start_crawler(Name=glue_crawler)# triggering  the glue crawler.
